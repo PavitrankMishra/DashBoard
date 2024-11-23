9072996e07828f31009ef6306ff0f81839d1f7ca
Pavitrank <pavitrank2002@gmail.com>
2024-11-23 22:13:09 +0530

    Updated the code for updating a single user

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const port = 3000;
 
-// Route
+// Route to get all the users
 
 app.get("/api/users", (req,res) => {
     const filePath = path.join(__dirname, "db.json");
@@ -19,6 +19,28 @@ app.get("/api/users", (req,res) => {
         }
     })
 })
+
+// Route to get a single user
+
+app.get("/api/users/:id", (req, res) => {
+    const filePath = path.join(__dirname, "db.json");
+    fs.readFile(filePath, "utf8", (err,data) => {
+        if(err) {
+            console.log("Error reading file: " + err);
+            res.status(500).send({error: "Unable to read file "});
+        } else {
+            const users = JSON.parse(data).users;
+            const user = users.find((u) => u.id === parseInt(req.params.id));
+            if(user) {
+                res.json(user);
+            }else {
+                res.status(404).send({error: "User not found"});
+            }
+        }
+    })
+});
+
+
 app.get("/", (req, res) => {
   res.send({ message: "Response from server! " });
 });
