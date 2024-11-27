21d0bcfb1146b3bce846014d569252e38b37b227
Pavitrank <pavitrank2002@gmail.com>
2024-11-27 07:29:47 +0530

    Initialised a component for navigation and a module for css file.

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,9 @@
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return <div className={styles.container}>
+  This is the navigation component
+  </div>;
+};
+
+export default Navigation;
